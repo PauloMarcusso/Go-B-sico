8844951a8b459fcd61124c59f11fc4549591092e
paulo_marcusso <paulo_marcusso@hst.br>
2022-05-05 12:59:33 -0300

    registrando e lendo logs

--- hello/hello.go
+++ hello/hello.go
@@ -4,8 +4,10 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -26,6 +28,7 @@ func main() {
 			iniciarMonitoramento()
 		case 2:
 			fmt.Println("Exibindo Logs...")
+			imprimirLogs()
 		case 0:
 			fmt.Println("Saindo do programa...")
 			os.Exit(0)
@@ -50,6 +53,29 @@ func iniciarMonitoramento() {
 	}
 
 	fmt.Println("")
+}
+
+func registrarLog(site string, status bool) {
+	arquivo, err := os.OpenFile("log.txt", os.O_CREATE|os.O_RDWR|os.O_APPEND, 0666)
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+	}
+
+	//Ver documentação para formato de data/hora
+	arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05") + " - " + site + " - online: " + strconv.FormatBool(status) + "\n")
+
+	arquivo.Close()
+}
+
+func imprimirLogs() {
+	arquivo, err := ioutil.ReadFile("log.txt")
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+	}
+
+	fmt.Println(string(arquivo))
 }
 
 func lerSitesDoArquivo() []string {
@@ -89,8 +115,10 @@ func testaSite(site string) {
 
 	if response.StatusCode == 200 {
 		fmt.Println("Site ", site, "foi carregado com sucesso!")
+		registrarLog(site, true)
 	} else {
 		fmt.Println("Site:", site, "está com problemas. Status Code:", response.StatusCode)
+		registrarLog(site, false)
 	}
 }
 
